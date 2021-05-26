39fded42e59eedce95a9475cb8c4f1b5cb786c80
TopGek99 <arowe890@gmail.com>
2021-05-27 01:55:22 +0930

    sign up and login functionality

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,7 +5,12 @@ module.exports = {
   find: function (req, res) {
     console.log(req.body);
     db.User.findOne({ email: req.body.email })
-      .then((dbModel) => res.json(dbModel))
+      .then((dbModel) => {
+        console.log(dbModel);
+        if (db.User.encryptPassword(req.body.password) == dbModel.password) {
+          res.json(dbModel);
+        }
+      })
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
